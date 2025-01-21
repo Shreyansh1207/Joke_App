6e38b350be77d785dcc90971eeb354e5321b54dd
Shreyansh1207 <131813012+Shreyansh1207@users.noreply.github.com>
2025-01-21 07:17:34 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const url="https://v2.jokeapi.dev/joke/Any";
-const btn1=document.querySelector("button");
+const btn1=document.getElementById("button");
+const btn2=document.getElementById("button2");
 let cur;
 const getSetup=async()=>{
     let response=await fetch(url);
@@ -13,7 +14,7 @@ const getDelivery=async()=>{
     return cur || "";
 }
 
-document.addEventListener("DOMContentLoaded",async () => {
+let z=document.addEventListener("DOMContentLoaded",async () => {
     let screen = document.querySelector("h1");
     screen.innerHTML = await getSetup();
 
@@ -23,3 +24,9 @@ const answer=async()=>{
     screen2.innerHTML = await getDelivery();
 }
 btn1.addEventListener("click",answer);
+btn2.addEventListener("click", async () => {
+    let screen = document.querySelector("h1");
+    let screen2 = document.querySelector("h2");
+    screen2.innerHTML = "";
+    screen.innerHTML = await getSetup();
+});
